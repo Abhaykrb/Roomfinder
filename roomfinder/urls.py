40e2b233f8aabd09b9cc712e110b5b5feb9f0281
Abhaykrb <65072557+Abhaykrb@users.noreply.github.com>
2020-11-28 09:36:42 +0545

    Update urls.py

--- roomfinder/urls.py
+++ roomfinder/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import include 
 from .views import base,rooms_datasets,showrooms,helpp,userinput
 from django.urls import path
-#from .models import finded_rooms
-
 urlpatterns= [
-   
     path('', base.as_view(), name = 'base'),
     path('findrooms', rooms_datasets, name="roomsfound"), 
     path('showrooms', showrooms, name='showrooms'),
