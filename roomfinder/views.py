438f2041300ec5aa88e8411f20f8d508699e58cb
Abhaykrb <65072557+Abhaykrb@users.noreply.github.com>
2020-11-28 09:35:22 +0545

    Update views.py

--- roomfinder/views.py
+++ roomfinder/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.http import HttpResponse
-from .models import finded_rooms
-from django.contrib.gis.measure import D 
+from .models import getroom
+#from django.contrib.gis.measure import D 
 from django.contrib.gis.geos import Point
 
 #Shows directly homepage
@@ -19,5 +19,22 @@ def showrooms(request):
 
 def helpp(request):
     return render(request,'help.html')
+
+def userinput(request):
+    if request.method == 'POST' :  
+        name = request.POST.get('namee')#accessing 'name'value entered by using in userinput.html   
+        coordinates = request.POST.get('koordinatee')
+        coords = coordinates.split(',')
+        point = Point(float(coords[0]), float(coords[1]))
+        phonenumber=request.POST.get('phnumm')
+        flooravailable=request.POST.get('flooorr')
+        availableroomsno=request.POST.get('roomsnumm')
+        rentratee = request.POST.get('roomratee')
+        store = getroom(namee=name, koordinatee=point,phnumm=phonenumber,flooorr=flooravailable,roomsnumm=availableroomsno, roomratee=rentratee)#inserting userinput value into models field
+        store.save()# saves these values of store in getroom table database directly
+        return redirect('base')#takes to homepage
+    else:
+        # GET, generate unbound (blank) form
+        return render(request,'userinput.html')
           
     
