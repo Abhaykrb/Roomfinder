1c20b08cf5a251c805e129e6082780603d357fca
Abhaykrb <65072557+Abhaykrb@users.noreply.github.com>
2020-11-28 09:37:51 +0545

    Update models.py

--- roomfinder/models.py
+++ roomfinder/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.gis.db import models
 from django.db.models import Manager as GeoManager
 # Create your models here.
-
-class finded_rooms(models.Model):
-    name=models.CharField(max_length=20)
-    koordinate=models.PointField(srid=4326)
-    room_rate=models.IntegerField()
-    des=models.TextField() 
+class getroom(models.Model):
+    namee=models.CharField(max_length=20)
+    koordinatee=models.PointField(srid=4326,geography=True)
+    phnumm=models.BigIntegerField()
+    flooorr=models.CharField(max_length=16)
+    roomsnumm=models.IntegerField()
+    roomratee=models.IntegerField()
     objects = GeoManager()
     
 def __unicode__(self):                #constructor
-        return self.name
+        return self.phnumm
+
+
